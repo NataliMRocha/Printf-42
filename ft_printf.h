d6ff293964218c5e6f65457473bc989a34dd3b30
Natali Moreira Rocha <namoreir@c3r3p4.42sp.org.br>
2023-08-24 15:23:00 -0300

    ft_printf_header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: namoreir <namoreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 10:05:40 by namoreir          #+#    #+#             */
-/*   Updated: 2023/08/24 12:39:48 by namoreir         ###   ########.fr       */
+/*   Updated: 2023/08/24 15:20:34 by namoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@
 # include <stdarg.h>
 
 int	ft_printf(const char *format, ...);
+int	ft_checkformat(char type, va_list arg);
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putnbr(int nb);
 int	ft_putnbr_uns(unsigned int nb);
-int	ft_putnbr_hx_lower(unsigned int hx);
-int	ft_putnbr_hx_upper(unsigned int hx);
-int	ft_putptr(unsigned long int ptr);
-int	ft_checkformat(const char *format, va_list	argptr, int size);
+int	ft_putnbr_hx(unsigned int hx, char *base);
+int	ft_putptr(unsigned long ptr, char *base);
 
 #endif
