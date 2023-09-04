cdc1b51c8d5c76c2ecfb590757e6f7b8d5fd7377
Natali Moreira Rocha <namoreir@c3r2p5.42sp.org.br>
2023-09-04 11:09:49 -0300

    ft_putnbr_bonus

--- ft_putnbr_bonus.c
+++ ft_putnbr_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: natali <natali@student.42.fr>              +#+  +:+       +#+        */
+/*   By: namoreir <namoreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 10:30:22 by namoreir          #+#    #+#             */
-/*   Updated: 2023/08/26 09:13:05 by natali           ###   ########.fr       */
+/*   Updated: 2023/09/04 10:51:56 by namoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,31 @@ int	ft_putnbr_hx(unsigned int hx, char *base)
 	{
 		size += ft_putnbr_hx(hx / 16, base);
 		size += ft_putchar(base[hx % 16]);
+	}
+	return (size);
+}
+
+int	ft_putptr(unsigned long ptr, char *base)
+{
+	int			size;
+
+	if ((void *)ptr == NULL || ptr == 0)
+	{
+		write(1, "(nil)", 5);
+		size = 5;
+		return (size);
+	}
+	size = 0;
+	if (ptr < 16)
+	{
+		write(1, "0x", 2);
+		size = 2;
+		size += ft_putchar(base[ptr % 16]);
+	}
+	else if (ptr >= 16)
+	{
+		size += ft_putptr(ptr / 16, base);
+		size += ft_putchar(base[ptr % 16]);
 	}
 	return (size);
 }
